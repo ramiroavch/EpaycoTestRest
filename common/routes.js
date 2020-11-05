2d074e997560898687a088dec0ddade0e8c1a1b5
RAM <ramirovargas.dev@gmail.com>
2020-11-05 19:08:42 -0400

    Feature(app): Agrega liberia de validators

--- common/routes.js
+++ common/routes.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const router = express.Router();
+const routes = express.Router();
 const clientRoutes = require('../src/client/infraestructure/client-routes');
 const paymentRoutes = require('../src/payment/infraestructure/payment-routes');
 
-router.use('/client',clientRoutes);
-router.use('/payment',paymentRoutes);
+routes.use('/client',clientRoutes);
+routes.use('/payment',paymentRoutes);
 
-router.get('/',(req,res)=>{
+routes.get('/',(req,res)=>{
     res.send("default");
 })
 
-router.get('*',(req,res)=>{
+routes.get('*',(req,res)=>{
     res.send("Not Found");
 })
 
-module.exports = router;
+module.exports = routes;
