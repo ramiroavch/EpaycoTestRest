de35a1b213b3a32a4d1174c106e79e692f862a3e
RAM <ramirovargas.dev@gmail.com>
2020-11-05 19:13:05 -0400

    Feature(client): Agrega validators a las rutas

--- src/client/infraestructure/client-routes.js
+++ src/client/infraestructure/client-routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const clientController = require('./client-controller');
 const clientRoutes = express.Router();
+const {registerValidator,resultRegisterValidator,payWalletValidator,
+        consultWallet} = require ('./client-validator');
 
-clientRoutes.post('/',clientController.registerClient)
+clientRoutes.post('/',registerValidator,resultRegisterValidator,clientController.registerClient);
+clientRoutes.post('/wallet',payWalletValidator,resultRegisterValidator,clientController.payWallet);
+clientRoutes.get('/wallet',consultWallet,resultRegisterValidator,clientController.consultWallet);
 
 module.exports = clientRoutes;
