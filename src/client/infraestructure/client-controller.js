a24b61855f8320a82ab9a2446ed79c63a8c14bb6
RAM <ramirovargas.dev@gmail.com>
2020-11-05 23:12:08 -0400

    Feature(client): Agrega registro de cliente

--- src/client/infraestructure/client-controller.js
+++ src/client/infraestructure/client-controller.js
@@ -1,9 +1,20 @@
-const registerClient = (req,res)=>{
-    res.send("registro de cliente");
+const clientService = require('../application/client-service');
+
+const registerClient = async (req,res)=>{
+    try{
+        const {document,name,lastname,email,phone} = req.body;
+        const client = await clientService.createClient(document,name,lastname,email,phone);
+        res.status(200).send(client);
+    }
+    catch(err){
+        res.status(404).send({success:false,message:err.message});
+    }
 };
+
 const payWallet = (req,res)=>{
     res.send("recarga de billetera");
 }
+
 const consultWallet = (req,res)=>{
     res.send("obtener saldo de billetera")
 }
