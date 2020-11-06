35d6375c4e487f0d3cfce0bdbedb3bcdfb256644
RAM <ramirovargas.dev@gmail.com>
2020-11-06 01:06:26 -0400

    Refactor(app): Eliminacion de imports sin usar

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const databaseConnection= require("./common/database-connection");
 
 //--importing routes
 const routes = require('./common/routes');
@@ -8,7 +7,7 @@ const routes = require('./common/routes');
 //---settings
 app.set('port', process.env.PORT || 3000);
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
 require('./common/database-connection');
 app.use('/',routes);
 
