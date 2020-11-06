5d7ba5e57f8f2a64549e8b816be9b841de922096
RAM <ramirovargas.dev@gmail.com>
2020-11-06 03:26:45 -0400

    Feature(payment): Agrega metodo de autenticación de pago

--- src/payment/infraestructure/payment-controller.js
+++ src/payment/infraestructure/payment-controller.js
@@ -1,9 +1,31 @@
-const makePayment = (req,res)=>{
-    res.send("payment successful");
+const paymentService = require('../application/payment-service');
+
+const makePayment = async (req,res)=>{
+    try{
+        const {document,amount} = req.body;
+        const payment = await paymentService.makePayment(document,amount,54201234);
+        res.status(200).send(payment);
+    }
+    catch(err){
+        if(err.code!=null)
+            res.status(err.code).send(err.message);
+        else
+            res.status(500).send(err.message);
+    }
 }
 
-const authPayment = (req,res)=>{
-    res.send("autenticacion de pago");
+const authPayment = async(req,res)=>{
+    try{
+        const {document,token} = req.body;
+        const payment = await paymentService.authPayment(document,token);
+        res.status(200).send(payment);
+    }
+    catch(err){
+        if(err.code!=null)
+            res.status(err.code).send(err.message);
+        else
+            res.status(500).send(err.message);
+    }
 }
 
 module.exports = {
