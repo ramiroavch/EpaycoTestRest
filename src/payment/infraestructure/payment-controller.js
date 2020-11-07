d496a4b45501252dbcc8d8333669d11447c5a2f9
RAM <ramirovargas.dev@gmail.com>
2020-11-06 23:04:23 -0400

    Agrega generación de token de 6 digitos

--- src/payment/infraestructure/payment-controller.js
+++ src/payment/infraestructure/payment-controller.js
@@ -3,7 +3,7 @@ const paymentService = require('../application/payment-service');
 const makePayment = async (req,res)=>{
     try{
         const {document,amount} = req.body;
-        const payment = await paymentService.makePayment(document,amount,54201234);
+        const payment = await paymentService.makePayment(document,amount,getRandomdigits(6));
         res.status(200).send(payment);
     }
     catch(err){
@@ -26,6 +26,15 @@ const authPayment = async(req,res)=>{
         else
             res.status(500).send(err.message);
     }
+}
+
+const getRandomdigits= (length)=> {
+    var randomChars = '01234567899876543210';
+    var result = '';
+    for ( var i = 0; i < length; i++ ) {
+        result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+    return result;
 }
 
 module.exports = {
