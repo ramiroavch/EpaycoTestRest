ec5596721452c21a4afd4ea34b4aeb0470a27ece
RAM <ramirovargas.dev@gmail.com>
2020-11-10 00:54:41 -0400

    Refactor(app): eliminación de comentarios

--- app.js
+++ app.js
@@ -8,17 +8,9 @@ const routes = require('./common/routes');
 //---settings
 app.set('port', process.env.PORT || 3001);
 app.use(express.json());
-
 app.use(cors());
-
 require('./common/database-connection');
 app.use('/',routes);
-
-//---middlewares
-
-//---routes
-
-//---staticfiles
 
 app.listen(app.get('port'),()=>{
     console.log('server on port',app.get('port'));
